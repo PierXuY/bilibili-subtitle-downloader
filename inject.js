e90ee3ec5b9c6e68de4f02440945af0fbdb3c811
PierXuY <98803367+PierXuY@users.noreply.github.com>
2025-04-13 10:37:40 +0800

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,5 +1,11 @@
 (function () {
-    let value = JSON.parse(JSON.stringify(window.__INITIAL_STATE__));
+    let value = null;
+    try {
+        value = JSON.parse(JSON.stringify(window.__INITIAL_STATE__));
+    }
+    catch (e) {
+        console.error('window.__INITIAL_STATE__:', e);
+    }
     // 触发事件并传递数据
     document.dispatchEvent(new CustomEvent('FromPage', {
         detail: value,
